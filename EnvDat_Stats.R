1f441ea01f3725f2116fe0809845be9a2a22990b
ekunselman <56281882+ekunselman@users.noreply.github.com>
2021-10-04 12:12:53 -0700

    Add files via upload

--- EnvDat_Stats.R
+++ EnvDat_Stats.R
@@ -4,6 +4,7 @@ head(EnvDat)
 str(EnvDat)
 library(ggplot2)
 library(ggpubr)
+#plot
 SD<-ggplot(EnvDat, aes(x=Site, y=DO)) +
   geom_boxplot()
 ST<-ggplot(EnvDat, aes(x=Site, y=Temp)) +
@@ -25,6 +26,7 @@ SHS<-ggplot(EnvDat, aes(x=Site_Habitat, y=Sal)) +
 ggarrange(SD, ST, SS, HD, HT, HS, SHD, SHT, SHS,
           ncol = 3, nrow = 3)
 
+#Run two way anovas and post hoc tests
 mixdAOV_DO<-aov(DO~Site*Habitat, data=EnvDat)
 summary(mixdAOV_DO)
 TukeyHSD(mixdAOV_DO)
@@ -36,3 +38,35 @@ TukeyHSD(mixdAOV_T)
 mixdAOV_Sal<-aov(Sal~Site*Habitat, data=EnvDat)
 summary(mixdAOV_Sal)
 TukeyHSD(mixdAOV_Sal)
+
+#Calculate means
+aggregate(EnvDat$Sal, list(EnvDat$Site_Habitat), FUN=mean)
+Group.1        x
+1     CI.Bare       NA
+2 CI.Eelgrass       NA
+3     FB.Bare 31.94068
+4 FB.Eelgrass       NA
+5     PG.Bare 32.57594
+6 PG.Eelgrass 32.95038
+7     SK.Bare 31.69843
+8 SK.Eelgrass 30.83543
+aggregate(EnvDat$DO, list(EnvDat$Site_Habitat), FUN=mean)
+Group.1        x
+1     CI.Bare 9.534646
+2 CI.Eelgrass 8.993701
+3     FB.Bare 7.206780
+4 FB.Eelgrass 7.205932
+5     PG.Bare 8.541353
+6 PG.Eelgrass 8.737594
+7     SK.Bare 2.739370
+8 SK.Eelgrass 2.356693
+aggregate(EnvDat$Temp, list(EnvDat$Site_Habitat), FUN=mean)
+Group.1        x
+1     CI.Bare 16.75354
+2 CI.Eelgrass 16.68189
+3     FB.Bare 14.44068
+4 FB.Eelgrass 14.37712
+5     PG.Bare 15.94511
+6 PG.Eelgrass 15.92105
+7     SK.Bare 11.45039
+8 SK.Eelgrass 11.33937
